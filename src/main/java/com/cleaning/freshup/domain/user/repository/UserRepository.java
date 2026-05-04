db6cbeca3449376df9acfb0efcbde1e611f70302
elena <tool0923@naver.com>
2026-05-04 21:45:37 +0900

    로그인 refreshToken 재발급

--- src/main/java/com/cleaning/freshup/domain/user/repository/UserRepository.java
+++ src/main/java/com/cleaning/freshup/domain/user/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.cleaning.freshup.domain.user.repository;
 
 import com.cleaning.freshup.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Modifying;
-// import org.springframework.data.jpa.repository.Query;
-// import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
